e8fbb7af90f0114dd40f3516153316c7a12e5cbc
grubergen <iluxa@grubergen.ru>
2015-06-19 06:27:34 +0300

    Update ticket.java

--- ticket.java
+++ ticket.java
@@ -1,5 +1,10 @@
 package happyTicket;
 
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
   		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
